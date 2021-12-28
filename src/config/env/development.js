db74db9312253b660e895284276e60a1701a4e0e
Oluwafemi  Babayemi <femi.gabs@gmail.com>
2021-12-28 12:10:14 +0100

    env update

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -5,4 +5,5 @@ export default {
   APP_HOST: process.env.SWAPI_APP_HOST,
   APP_PORT: process.env.SWAPI_APP_PORT,
   API_VERSION: process.env.SWAPI_API_VERSION,
+  NODE_ENV: process.env.SWAPI_NODE_ENV,
 };
